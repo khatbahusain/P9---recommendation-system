fc3f16bac016a0c02e2b257cd13729095d9634b4
khatbahusain <khatba.husain@yahoo.com>
2023-02-21 14:19:21 +0100

    edit function

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get('name')
+    if name:
+        return func.HttpResponse(f"Hello world hahaha, {name}!")
+    else:
+        return func.HttpResponse("Please provide a name parameter in the query string.", status_code=400)

b89d2143a855a66d387a2dd5bbb9f320a983abe4
khatbahusain <khatba.husain@yahoo.com>
2023-02-21 16:09:51 +0100

    test ci cd

--- __init__.py
+++ __init__.py
@@ -1,8 +1,24 @@
+import logging
+
 import azure.functions as func
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
     name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
     if name:
-        return func.HttpResponse(f"Helloooooooo world mais sans hahaha, {name}!")
+        return func.HttpResponse(f"Hello, and welecome from ci cd {name}. This HTTP triggered function executed successfully.")
     else:
-        return func.HttpResponse("Please provide a name parameter in the query string.", status_code=400)
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )

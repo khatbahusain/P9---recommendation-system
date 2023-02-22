ad325c1d58b2e34cfffd0b802a74a0593fb40d0d
khatbahusain <khatba.husain@yahoo.com>
2023-02-22 17:54:33 +0100

    test small model

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,24 +1,50 @@
 import logging
-
 import azure.functions as func
+import pickle
+import pandas as pd
+from heapq import nlargest
+#test
+clicks_df = pd.read_csv('clicks_df.csv')
+with open('model_small.pkl', 'rb') as file:
+    model = pickle.load(file)
 
+def get_top_n_articles_for_user(user_id, n=5):
+    
+    articles = list(range(364047))
+    # Get list of articles that the user has already clicked on
+    articles_read = clicks_df.loc[clicks_df['user_id'] == user_id, 'click_article_id'].tolist()#[:1]
 
+    # Remove articles that the user has already read from the articles index
+    articles_to_recommend = [article for article in articles if article not in articles_read]
+        
+    # Use collaborative filtering to predict user ratings for remaining articles
+    user_ratings = model.test([(user_id, article_id, 0) for article_id in articles_to_recommend], verbose=False)
+    
+    results = {rating.iid: rating.est for rating in user_ratings}
+
+    # Return the top n articles with the highest predicted ratings
+    return nlargest(n, results, key=results.get)
+
+#test
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    user_id = req.params.get('user_id')
+    if not user_id:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            user_id = req_body.get('user_id')
 
-    if name:
-        return func.HttpResponse("243431,34242,5324,7543,42432")
+    if user_id:
+        
+        result = get_top_n_articles_for_user(user_id, n=5)
+        return func.HttpResponse(str(result))
+    
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass an id in the query string or in the request body for a personalized response.",
              status_code=200
         )

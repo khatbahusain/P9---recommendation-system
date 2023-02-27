2b630e798851e11623785064145aef916fa840e5
khatbahusain <khatba.husain@yahoo.com>
2023-02-27 19:44:34 +0100

    update model

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -35,13 +35,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     user_id = req.params.get('user_id')
-    if not user_id:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            user_id = req_body.get('user_id')
 
     if user_id:
         
